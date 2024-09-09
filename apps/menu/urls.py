07d8c6d1aec6fb13db58c4c8187161595047dcc4
samcasas <samycasas1@gmail.com>
2024-09-08 19:36:49 -0600

    promociones  terminadas

--- apps/menu/urls.py
+++ apps/menu/urls.py
@@ -2,16 +2,45 @@ from django.urls import path
 from .views import MenuView
 
 urlpatterns = [
+    
+    
+    #------------------------------------CATEGORIES----------------------------------
+
     #POST
     path('add-category', MenuView.as_view(action='add_category'), name='add-category'),
     path('put-category', MenuView.as_view(action='update_category'), name='put-category'),
-    path('add-product', MenuView.as_view(action='add_product'), name='add-product'),
-    path('add-promotion', MenuView.as_view(action='add_promotion'), name='add-promotion'),
     path('upload-image-category', MenuView.as_view(action='upload_image_category'), name='upload-image-category'),
 
     #GET
-    path('get-categories', MenuView.as_view(action='get_categories'), name='get_categories'),
-   
+    path('get-categories', MenuView.as_view(action='get_categories'), name='get-categories'),
+    
     #DELETE
     path('delete-category/<int:id>', MenuView.as_view(action='delete_category'), name='delete-category'),
+
+    #------------------------------------PROMOTIONS----------------------------------
+    #POST
+    path('add-promotion', MenuView.as_view(action='add_promotion'), name='add-promotion'),
+    path('put-promotion', MenuView.as_view(action='update_promotion'), name='put-promotion'),
+    path('upload-image-promotion', MenuView.as_view(action='upload_image_promotion'), name='upload-image-promotion'),
+
+    #GET
+    path('get-promotions', MenuView.as_view(action='get_promotions'), name='get-promotions'),
+    
+    #DELETE
+    path('delete-promotion/<int:id>', MenuView.as_view(action='delete_promotion'), name='delete-promotion'),
+    
+    #------------------------------------PRODUCTS----------------------------------
+    path('add-product', MenuView.as_view(action='add_product'), name='add-product'),
+    path('put-product', MenuView.as_view(action='update_product'), name='put-product'),
+    path('upload-image-product', MenuView.as_view(action='upload_image_product'), name='upload-image-product'),
+    
+    #GET
+    path('get-products', MenuView.as_view(action='get_products'), name='get-products'),
+    
+    #DELETE
+    path('delete-product/<int:id>', MenuView.as_view(action='delete_product'), name='delete-product'),
+    
+    
+   
+    
 ]
--- apps/menu/views.py
+++ apps/menu/views.py
@@ -26,22 +26,38 @@ class MenuView(APIView, Helper):
         self.action = kwargs.get('action')
 
     def post(self, request, *args, **kwargs):
+        #categories
         if self.action == 'add_category':
             return self.addCategory(request)
         elif self.action == 'update_category':
             return self.updateCategory(request)
-        elif self.action == 'add_product':
-            return self.addProduct(request)
-        elif self.action == 'add_promotion':
-            return self.addPromotion(request)
         elif self.action == 'upload_image_category':
             return self.uploadImageCategory(request)
+        
+        #products
+        elif self.action == 'add_product':
+            return self.addProduct(request)
+        elif self.action == 'update_product':
+            return self.updateProduct(request)
+        elif self.action == 'upload_image_product':
+            return self.uploadImageProduct(request)
+        
+        #promotions
+        elif self.action == 'add_promotion':
+            return self.addPromotion(request)
+        elif self.action == 'update_promotion':
+            return self.updatePromotion(request)
+        elif self.action == 'upload_image_promotion':
+            return self.uploadImagePromotion(request)
+
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
     def get(self, request, id=None, *args, **kwargs):
         if self.action == 'get_categories':
             return self.getCategories(request)
+        elif self.action == 'get_promotions':
+            return self.getPromotions(request)
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -137,12 +153,6 @@ class MenuView(APIView, Helper):
             return Response(self.responseRequest(False, f'Error al subir la imagen: {str(e)}', {}, 500), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
 
-    def addProduct(self, request):
-        pass
-
-    def addPromotion(self, request):
-        pass
-
     def getCategories(self, request):
         try:
             user_id = request.user.id
@@ -179,3 +189,121 @@ class MenuView(APIView, Helper):
             return Response(self.responseRequest(False, 'No se pudo encontrar la categoria.', {}, 404), status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
             return Response(self.responseRequest(False, f'Error: {str(e)}', {}, 500), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+    def addPromotion(self, request):
+        promotion_data = JSONParser().parse(request)
+        user_id = request.user.id
+        try:
+            promotion_data['user_id'] = user_id
+            promotion_data['status'] = 1 if promotion_data['status'] == True else 0
+            serializer = PromotionSerializer(data=promotion_data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(self.responseRequest(True, 'Promoción creada correctamente.', serializer.data, 201), status=status.HTTP_201_CREATED)
+            
+            return Response(self.responseRequest(False, 'No se pudo crear la promoción.', {}, 409), status=status.HTTP_409_CONFLICT)
+        
+        except Exception as e:
+            return Response(self.responseRequest(False, f'No se pudo crear la promoción. Error: {str(e)}', {}, 500), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    def updatePromotion(self, request):
+        promotion_data = JSONParser().parse(request)
+        user_id = request.user.id
+        try:
+            promotion = Promotion.objects.get(id=promotion_data['id'])
+            promotion_data['user_id'] = user_id
+            promotion_data['status'] = 1 if promotion_data['status'] == True else 0
+            serializer = PromotionSerializer(promotion, data=promotion_data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(self.responseRequest(True, 'Promoción actualizada correctamente.', serializer.data, 200), status=status.HTTP_200_OK)
+            
+            return Response(self.responseRequest(False, 'No se pudo actualizar la promoción.', serializer.errors, 409), status=status.HTTP_409_CONFLICT)
+        
+        except Promotion.DoesNotExist:
+            return Response(self.responseRequest(False, 'Promoción no encontrada.', {}, 404), status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as e:
+            return Response(self.responseRequest(False, f'No se pudo actualizar la promoción. Error: {str(e)}', {}, 500), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def uploadImagePromotion(self, request):
+        # Obtener el archivo de imagen
+        image_file = request.FILES.get('image')
+        promotion_id = request.data.get('promotion_id')
+
+        if not image_file:
+            return Response(self.responseRequest(False, 'No se encontró ninguna imagen en la solicitud.', {}, 400), status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            promotion = Promotion.objects.get(id=promotion_id)
+            # Subir la imagen a S3
+            s3_client = self.getS3Client()
+            
+            bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+            name = promotion.name
+            name = name.lower().replace(" ", "-") + f"-{promotion.id}"
+            file_key = f'images/promotions/{request.user.id}/{name}'  # Nombre del archivo en S3
+            
+            # Eliminar la imagen anterior si existe
+            if promotion.image != "":
+                old_file_key = promotion.image.split(f'https://{bucket_name}.s3.amazonaws.com/')[1]
+                s3_client.delete_object(Bucket=bucket_name, Key=old_file_key)
+
+            # Subir el archivo a S3
+            s3_client.upload_fileobj(image_file, bucket_name, file_key)
+            
+            # Obtener la URL de la imagen
+            image_url = f'https://{bucket_name}.s3.amazonaws.com/{file_key}'
+            
+            # Guardar la URL en el modelo
+            promotion.image = image_url
+            promotion.save()
+            
+            return Response(self.responseRequest(True, 'Imagen de promoción actualizada correctamente.', {'image_url': image_url}, 200), status=status.HTTP_200_OK)
+        
+        except Promotion.DoesNotExist:
+            return Response(self.responseRequest(False, 'No se encontró ninguna instancia de Promoción.', {}, 404), status=status.HTTP_404_NOT_FOUND)
+        
+        except Exception as e:
+            return Response(self.responseRequest(False, f'Error al subir la imagen: {str(e)}', {}, 500), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    def getPromotions(self, request):
+        try:
+            user_id = request.user.id
+            promotions = Promotion.objects.filter(user_id=user_id)
+            serializer = PromotionSerializer(promotions, many=True)
+            return Response(self.responseRequest(True, 'Promociones obtenidas correctamente.', serializer.data, 200), status=status.HTTP_200_OK)
+        except Promotion.DoesNotExist:
+            return Response(self.responseRequest(False, 'No se encontraron promociones.', {}, 404), status=status.HTTP_404_NOT_FOUND)
+
+    def deletePromotion(self, request, id):
+        user_id = request.user.id
+        try:
+            promotion = Promotion.objects.get(user_id=user_id, id=id)
+            
+            s3_client = self.getS3Client()
+            
+            bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+            name = promotion.name
+            name = name.lower().replace(" ", "-") + f"-{promotion.id}"
+            file_key = f'images/promotions/{request.user.id}/{name}'  # Nombre del archivo en S3
+            
+            # Eliminar la imagen anterior si existe
+            if promotion.image != "":
+                old_file_key = promotion.image.split(f'https://{bucket_name}.s3.amazonaws.com/')[1]
+                s3_client.delete_object(Bucket=bucket_name, Key=old_file_key)
+
+            promotion.delete()
+
+            return Response(self.responseRequest(True, 'Promoción eliminada correctamente.', {}, 200), status=status.HTTP_200_OK)
+        
+        except Promotion.DoesNotExist:
+            return Response(self.responseRequest(False, 'No se pudo encontrar la promoción.', {}, 404), status=status.HTTP_404_NOT_FOUND)
+        
+        except Exception as e:
+            return Response(self.responseRequest(False, f'Error al eliminar la promoción: {str(e)}', {}, 500), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+    def addProduct(self, request):
+        pass
